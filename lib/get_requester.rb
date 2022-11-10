aca3a6883259761c7ac9260c81b1ae2a6234e835
MurituMaina <vincent.maina@student.moringaschool.com>
2022-11-10 08:09:28 +0300

    commit tree 0eb7f441f8d5b5afdab839ad1ae274555c75bf85

--- lib/get_requester.rb
+++ lib/get_requester.rb
@@ -1,1 +1,11 @@
 # Write your code here
+class GetRequester
+URL = "https://learn-co-curriculum.github.io/json-site-example/endpoints/people.json"
+
+def get_response_body
+uri = URI.parse(URL)
+response = NET::HTTP.get_response(uri)
+response.body
+end
+
+end
